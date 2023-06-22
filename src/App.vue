da5d103d7938f14126332240268fb99719825cbe
zenenperaza <peraza@outlook.com>
2023-06-22 19:44:10 -0400

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,26 @@
 <template>
-<h1>{{ store.count }}</h1>
-<button v-on:click="store.increment">Add </button>
+<h1>Notas: </h1>
+<input type="text" name="" id="" v-model="note">
+
 </template>
 
-<script lang="ts" setup>
-import { useExample } from '@/store/useExample'
+<script lang="ts" >
+import { defineComponent,ref, watch } from "vue"
+export default defineComponent({
+  setup() {
+    const note = ref("")
+    watch(note, () => alert("se ha modificado el valor: " + note.value))
+    return {
+      note
+    }
+  }
 
-const store = useExample()
-store.init()
+})
+
+
 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
